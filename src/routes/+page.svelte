e8c8cf85a8f5134fad12f23afa3779e10c408014
Melvin A <melvinalmonte@gmail.com>
2022-10-03 18:53:02 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="title">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/second-page">go to second page</a>
---
+++ src/routes/second-page/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class='title'>Hello from home page</h1>
+<a href="/">go to home page</a>
